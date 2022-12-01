adf579d736f5992e6981baea87060d71d2abe438
Nadim <nahlenadim@gmail.com>
2022-12-01 15:41:52 +0200

    Backend-Node Create- Category Model

---
+++ backend-node/models/category.js
@@ -0,0 +1,39 @@
+const mongoose = require("mongoose");
+
+//category SCHEMA
+const CategorySchema = new Schema({
+    _id: {
+        type: Schema.ObjectId,
+        auto: true
+    },
+    name: {
+        type: String,
+        trim: true
+    },
+    slug: {
+        type: String,
+        slug: 'name',
+        unique: true
+    },
+    image: {
+
+    },
+    description: {
+        type: String,
+        trim: true
+    },
+    products: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: 'Product'
+        }
+    ],
+    updated: Date,
+    created: {
+        type: Date,
+        default: Date.now
+    }
+});
+
+//EXPORTING SCHEMA
+module.exports = mongoose.model("Category", CategorySchema);
