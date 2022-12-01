d67d2e5059e5ba9afc17f81b77bf7ce8214cc3e1
Nadim <nahlenadim@gmail.com>
2022-12-01 15:54:38 +0200

    Backend-Node Create- product routes

--- backend-node/app/routes.js
+++ backend-node/app/routes.js
@@ -4,16 +4,12 @@ const router = express.Router();
 
 // ROUTES
 
-
-
 //PRODUCT ROUTES
-router.post("/auth/addproduct", auth, addProduct);
-router.get("/auth/products", auth, getProduct);
-router.delete("/auth/delete/:id", auth, deleteProducts);
-router.patch("/auth/product/update/:id", auth, updateProduct);
+router.post("/addproduct", auth, addProduct);
+router.get("/products", auth, getProduct);
+router.delete("/delete/:id", auth, deleteProducts);
+router.patch("/product/update/:id", auth, updateProduct);
 
 
-//ADMIN
-router.post("/auth/login/admin", getAdmin, adminLogin)
 
 module.exports = router;
