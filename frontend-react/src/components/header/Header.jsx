3cca7fcdaf371e2975ca6c5b24bd9db0d893681b
Nadim <nahlenadim@gmail.com>
2022-12-03 15:43:59 +0200

    Fronted-React Create Header

--- frontend-react/src/components/header/Header.jsx
+++ frontend-react/src/components/header/Header.jsx
@@ -1,7 +1,15 @@
 import React from "react";
+import "./Header.css";
+import rest from "../../assets/img/restaurant.png";
 
 const Header = () => {
-  return <div className="header">Header</div>;
+  return (
+    <div className="header">
+      <div className="img-container">
+        <img src={rest} alt="" className="logo" />
+      </div>
+    </div>
+  );
 };
 
 export default Header;
