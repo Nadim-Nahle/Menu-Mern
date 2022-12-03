39351ebaf6c522c6f70119c5d426c32a1c802380
Nadim <nahlenadim@gmail.com>
2022-12-03 16:33:43 +0200

    Fronted-React Create Front Page Skeleton

--- frontend-react/src/pages/Home.jsx
+++ frontend-react/src/pages/Home.jsx
@@ -1,7 +1,170 @@
 import React from "react";
 
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <div className="home">
+      <div className="menu-items-container">
+        <div className="menu-items">
+          <div className="items">
+            <div>Chicken</div>
+            <div>Pizza</div>
+            <div>Burger</div>
+            <div>Juices</div>
+          </div>
+        </div>
+      </div>
+      <div className="categories">
+        <div className="category">
+          <div className="category-name">Burger</div>
+          <div className="items">
+            <div className="item">
+              <div className="pic-container"></div>
+              <div className="title-container"></div>
+            </div>
+            <div className="item">
+              <div className="pic-container"></div>
+              <div className="title-container"></div>
+            </div>
+            <div className="item">
+              <div className="pic-container"></div>
+              <div className="title-container"></div>
+            </div>
+            <div className="item">
+              <div className="pic-container"></div>
+              <div className="title-container"></div>
+            </div>
+            <div className="item">
+              <div className="pic-container"></div>
+              <div className="title-container"></div>
+            </div>
+            <div className="item">
+              <div className="pic-container"></div>
+              <div className="title-container"></div>
+            </div>
+            <div className="item">
+              <div className="pic-container"></div>
+              <div className="title-container"></div>
+            </div>
+            <div className="item">
+              <div className="pic-container"></div>
+              <div className="title-container"></div>
+            </div>
+          </div>
+        </div>
+        <div className="category">
+          <div className="category-name">Burger</div>
+          <div className="items">
+            <div className="item">
+              <div className="pic-container"></div>
+              <div className="title-container"></div>
+            </div>
+            <div className="item">
+              <div className="pic-container"></div>
+              <div className="title-container"></div>
+            </div>
+            <div className="item">
+              <div className="pic-container"></div>
+              <div className="title-container"></div>
+            </div>
+            <div className="item">
+              <div className="pic-container"></div>
+              <div className="title-container"></div>
+            </div>
+            <div className="item">
+              <div className="pic-container"></div>
+              <div className="title-container"></div>
+            </div>
+            <div className="item">
+              <div className="pic-container"></div>
+              <div className="title-container"></div>
+            </div>
+            <div className="item">
+              <div className="pic-container"></div>
+              <div className="title-container"></div>
+            </div>
+            <div className="item">
+              <div className="pic-container"></div>
+              <div className="title-container"></div>
+            </div>
+          </div>
+        </div>
+        <div className="category">
+          <div className="category-name">Burger</div>
+          <div className="items">
+            <div className="item">
+              <div className="pic-container"></div>
+              <div className="title-container"></div>
+            </div>
+            <div className="item">
+              <div className="pic-container"></div>
+              <div className="title-container"></div>
+            </div>
+            <div className="item">
+              <div className="pic-container"></div>
+              <div className="title-container"></div>
+            </div>
+            <div className="item">
+              <div className="pic-container"></div>
+              <div className="title-container"></div>
+            </div>
+            <div className="item">
+              <div className="pic-container"></div>
+              <div className="title-container"></div>
+            </div>
+            <div className="item">
+              <div className="pic-container"></div>
+              <div className="title-container"></div>
+            </div>
+            <div className="item">
+              <div className="pic-container"></div>
+              <div className="title-container"></div>
+            </div>
+            <div className="item">
+              <div className="pic-container"></div>
+              <div className="title-container"></div>
+            </div>
+          </div>
+        </div>
+        <div className="category">
+          <div className="category-name">Burger</div>
+          <div className="items">
+            <div className="item">
+              <div className="pic-container"></div>
+              <div className="title-container"></div>
+            </div>
+            <div className="item">
+              <div className="pic-container"></div>
+              <div className="title-container"></div>
+            </div>
+            <div className="item">
+              <div className="pic-container"></div>
+              <div className="title-container"></div>
+            </div>
+            <div className="item">
+              <div className="pic-container"></div>
+              <div className="title-container"></div>
+            </div>
+            <div className="item">
+              <div className="pic-container"></div>
+              <div className="title-container"></div>
+            </div>
+            <div className="item">
+              <div className="pic-container"></div>
+              <div className="title-container"></div>
+            </div>
+            <div className="item">
+              <div className="pic-container"></div>
+              <div className="title-container"></div>
+            </div>
+            <div className="item">
+              <div className="pic-container"></div>
+              <div className="title-container"></div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
