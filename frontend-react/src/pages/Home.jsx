91fc634b14e66bea8f61744fda0cd6a39823a391
Nadim <nahlenadim@gmail.com>
2022-12-03 18:56:56 +0200

    Fronted-React Fetch Data

--- frontend-react/src/pages/Home.jsx
+++ frontend-react/src/pages/Home.jsx
@@ -1,167 +1,51 @@
-import React from "react";
+import axios from "../api/axios";
+import React, { useEffect, useState } from "react";
 
 const Home = () => {
+  const [categories, setCategories] = useState([]);
+
+  const getCategories = async () => {
+    try {
+      const { data } = await axios.get("/categories");
+      setCategories(data);
+      console.log(data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  useEffect(() => {
+    getCategories();
+  }, []);
+
   return (
     <div className="home">
       <div className="menu-items-container">
         <div className="menu-items">
           <div className="items">
-            <div>Chicken</div>
-            <div>Pizza</div>
-            <div>Burger</div>
-            <div>Juices</div>
+            {categories?.map((c) => (
+              <div key={c?._id}>{c?.name}</div>
+            ))}
           </div>
         </div>
       </div>
       <div className="categories">
-        <div className="category">
-          <div className="category-name">Burger</div>
-          <div className="items">
-            <div className="item">
-              <div className="pic-container"></div>
-              <div className="title-container"></div>
-            </div>
-            <div className="item">
-              <div className="pic-container"></div>
-              <div className="title-container"></div>
-            </div>
-            <div className="item">
-              <div className="pic-container"></div>
-              <div className="title-container"></div>
-            </div>
-            <div className="item">
-              <div className="pic-container"></div>
-              <div className="title-container"></div>
-            </div>
-            <div className="item">
-              <div className="pic-container"></div>
-              <div className="title-container"></div>
-            </div>
-            <div className="item">
-              <div className="pic-container"></div>
-              <div className="title-container"></div>
-            </div>
-            <div className="item">
-              <div className="pic-container"></div>
-              <div className="title-container"></div>
-            </div>
-            <div className="item">
-              <div className="pic-container"></div>
-              <div className="title-container"></div>
+        {categories?.map((c) => (
+          <div className="category" key={c._id}>
+            <div className="category-name">{c?.name}</div>
+            <div className="items">
+              {c?.products?.map((p) => (
+                <div className="item">
+                  <div className="pic-container">
+                    <img className="pic" src={p.img} alt="" />
+                  </div>
+                  <div className="title-container">
+                    <div className="title">{p?.name}</div>
+                  </div>
+                </div>
+              ))}
             </div>
           </div>
-        </div>
-        <div className="category">
-          <div className="category-name">Burger</div>
-          <div className="items">
-            <div className="item">
-              <div className="pic-container"></div>
-              <div className="title-container"></div>
-            </div>
-            <div className="item">
-              <div className="pic-container"></div>
-              <div className="title-container"></div>
-            </div>
-            <div className="item">
-              <div className="pic-container"></div>
-              <div className="title-container"></div>
-            </div>
-            <div className="item">
-              <div className="pic-container"></div>
-              <div className="title-container"></div>
-            </div>
-            <div className="item">
-              <div className="pic-container"></div>
-              <div className="title-container"></div>
-            </div>
-            <div className="item">
-              <div className="pic-container"></div>
-              <div className="title-container"></div>
-            </div>
-            <div className="item">
-              <div className="pic-container"></div>
-              <div className="title-container"></div>
-            </div>
-            <div className="item">
-              <div className="pic-container"></div>
-              <div className="title-container"></div>
-            </div>
-          </div>
-        </div>
-        <div className="category">
-          <div className="category-name">Burger</div>
-          <div className="items">
-            <div className="item">
-              <div className="pic-container"></div>
-              <div className="title-container"></div>
-            </div>
-            <div className="item">
-              <div className="pic-container"></div>
-              <div className="title-container"></div>
-            </div>
-            <div className="item">
-              <div className="pic-container"></div>
-              <div className="title-container"></div>
-            </div>
-            <div className="item">
-              <div className="pic-container"></div>
-              <div className="title-container"></div>
-            </div>
-            <div className="item">
-              <div className="pic-container"></div>
-              <div className="title-container"></div>
-            </div>
-            <div className="item">
-              <div className="pic-container"></div>
-              <div className="title-container"></div>
-            </div>
-            <div className="item">
-              <div className="pic-container"></div>
-              <div className="title-container"></div>
-            </div>
-            <div className="item">
-              <div className="pic-container"></div>
-              <div className="title-container"></div>
-            </div>
-          </div>
-        </div>
-        <div className="category">
-          <div className="category-name">Burger</div>
-          <div className="items">
-            <div className="item">
-              <div className="pic-container"></div>
-              <div className="title-container"></div>
-            </div>
-            <div className="item">
-              <div className="pic-container"></div>
-              <div className="title-container"></div>
-            </div>
-            <div className="item">
-              <div className="pic-container"></div>
-              <div className="title-container"></div>
-            </div>
-            <div className="item">
-              <div className="pic-container"></div>
-              <div className="title-container"></div>
-            </div>
-            <div className="item">
-              <div className="pic-container"></div>
-              <div className="title-container"></div>
-            </div>
-            <div className="item">
-              <div className="pic-container"></div>
-              <div className="title-container"></div>
-            </div>
-            <div className="item">
-              <div className="pic-container"></div>
-              <div className="title-container"></div>
-            </div>
-            <div className="item">
-              <div className="pic-container"></div>
-              <div className="title-container"></div>
-            </div>
-          </div>
-        </div>
+        ))}
       </div>
     </div>
   );
