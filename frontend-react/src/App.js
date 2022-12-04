2e0e92d5dfe359923dee5035492884533d47f3f6
Nadim <nahlenadim@gmail.com>
2022-12-04 10:33:48 +0200

    Fronted-React Auto Scroll

--- frontend-react/src/App.js
+++ frontend-react/src/App.js
@@ -1,11 +1,14 @@
 import Header from "./components/header/Header";
 import Home from "./pages/Home";
+import { Route, Routes } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
       <Header />
-      <Home />
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+      </Routes>
     </div>
   );
 }
--- frontend-react/src/pages/Home.jsx
+++ frontend-react/src/pages/Home.jsx
@@ -1,8 +1,10 @@
 import axios from "../api/axios";
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 
 const Home = () => {
   const [categories, setCategories] = useState([]);
+  const scrollRef = useRef();
+  const [click, setClick] = useState(false);
 
   const getCategories = async () => {
     try {
@@ -16,6 +18,15 @@ const Home = () => {
   useEffect(() => {
     getCategories();
   }, []);
+  const scrollTo = (id) => {
+    let element = document.getElementById(id);
+    element.scrollIntoView({
+      behavior: "smooth",
+      block: "start",
+      inline: "nearest",
+    });
+    setClick(!click);
+  };
 
   return (
     <div className="home">
@@ -23,23 +34,34 @@ const Home = () => {
         <div className="menu-items">
           <div className="items">
             {categories?.map((c) => (
-              <div key={c?._id}>{c?.name}</div>
+              <div
+                className="menu-scroll"
+                key={c?._id}
+                onClick={() => scrollTo(`${c?.slug}`)}
+              >
+                {c?.name}
+              </div>
             ))}
           </div>
         </div>
       </div>
       <div className="categories">
         {categories?.map((c) => (
-          <div className="category" key={c._id}>
+          <div className="category" key={c?._id} id={c?.slug}>
             <div className="category-name">{c?.name}</div>
             <div className="items">
               {c?.products?.map((p) => (
-                <div className="item">
+                <div className="item" key={p?._id}>
                   <div className="pic-container">
                     <img className="pic" src={p.img} alt="" />
                   </div>
                   <div className="title-container">
                     <div className="title">{p?.name}</div>
+                  </div>
+                  <div className="title-container">
+                    <div className="title">Price:</div>
+                    <div className="title">{` ${p?.price}`}</div>
+                    <div className="title">$</div>
                   </div>
                 </div>
               ))}
